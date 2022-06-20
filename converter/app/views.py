435b6911c1864bc35972d6db89ce66cdb4129a63
Carlos Moerte <61364206+deadshumz@users.noreply.github.com>
2022-06-20 13:01:06 +0400

    Little fixes

--- converter/app/views.py
+++ converter/app/views.py
@@ -3,17 +3,18 @@ from django.views import View
 import requests
 
 class Converter(View):
+    template = 'app/index.html'
     api_response = requests.get('https://api.exchangerate-api.com/v4/latest/usd').json()
     currencies = api_response.get('rates')
 
     def get(self, request):
-        context = {'currencies': currencies}
-        return render(request, 'app/index.html', context)
+        context = {'currencies': self.currencies}
+        return render(request, self.template, context)
 
     def post(self, request):
         curr1 = request.POST['currencies_option1']
         curr2 = request.POST['currencies_option2']
         amount = request.POST['amount']
-        price = round(currencies[curr2] / currencies[curr1] * float(amount), 2)
-        context = {'currencies': currencies, 'price': price, 'curr1': curr1, 'curr2': curr2, 'amount': amount}
-        return render(request, 'app/index.html', context)
+        price = round(self.currencies[curr2] / self.currencies[curr1] * float(amount), 2)
+        context = {'currencies': self.currencies, 'price': price, 'curr1': curr1, 'curr2': curr2, 'amount': amount}
+        return render(request, self.template, context)
